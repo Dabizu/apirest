ac892ff31b51ae59a52b336f57fa555117c0b374
Dabizu Garushia <dabizu100@gmail.com>
2022-10-31 01:44:09 -0600

    Actualizar Array de json

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.use(cors());
 app.get("/",(req,res)=>{
     res.send("hola chido")
 })
+app.get("/jsonArray",(req,res)=>{
+    res.json('[{"name": "Molecule Man","age": 29,"secretIdentity": "Dan Jukes","powers": ["Radiation resistance","Turning tiny","Radiation blast"]},{"name": "Madame Uppercut","age": 39,"secretIdentity": "Jane Wilson","powers": ["Million tonne punch","Damage resistance","Superhuman reflexes"]}]')
+})
 app.listen(process.env.PORT || 3000,()=>{
     console.log("servidor corriendo");
 })
